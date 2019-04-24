7227644bab6b01ae2c197e46bb925350cb7f8d27
Roman Kislov <roman@kislovs.ru>
2019-04-25 00:17:52 +0500

    The skeleton

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
-const app = express()
 const morgan = require('morgan')
+const path = require('path')
+const mainRouter = require('./routes/main')
 
+const publicPath = path.join(__dirname,'/public')
+const viewsPath = path.join(__dirname,'/views')
+const app = express()
 
-
+app.use(morgan('combined'))
+app.set("view engine", "hbs")
+app.set('views'), viewsPath
+app.use(express.static(publicPath))
+app.use(mainRouter)
 
 
 module.exports = app
